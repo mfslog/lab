5eec4b89d65898485649ab827a820e2979c43b65
mfslog <jerry.chau@qq.com>
2019-11-15 01:09:18 +0800

    feat:sync

--- go/kit/cmd/cmd.go
+++ go/kit/cmd/cmd.go
@@ -1,11 +1,41 @@
 package cmd
 
-
-
-var (
-
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
 )
 
-func Execute(){
+var (
+	RootCmd = &cobra.Command{
+		Use:"",
+		Run: func(cmd *cobra.Command, args []string) {
+			
+		},
+	}
 
+	versionCmd = &cobra.Command{
+		Use:"version",
+		Short:"show version info",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("version: %d.%d.%d.%d\n",MAJOR,MINOR, PATCH, BUILD)
+			os.Exit(0)
+		},
+	}
+)
+
+
+
+
+
+
+
+
+
+func init(){
+	RootCmd.AddCommand(versionCmd)
+}
+
+func Execute()error{
+	return RootCmd.Execute()
 }

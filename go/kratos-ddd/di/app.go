c4e6574214f32a17bf8a5735ab985ca0de2f5eb8
mfslog <jerry.chau@qq.com>
2020-03-02 10:37:39 +0800

    feat:modify ip get

--- go/kratos-ddd/di/app.go
+++ go/kratos-ddd/di/app.go
@@ -9,11 +9,11 @@ import (
 	"github.com/bilibili/kratos/pkg/naming"
 	"github.com/bilibili/kratos/pkg/naming/discovery"
 	"github.com/bilibili/kratos/pkg/naming/etcd"
+	"github.com/bilibili/kratos/pkg/net/ip"
 	"github.com/bilibili/kratos/pkg/net/rpc/warden"
 	"github.com/mfslog/kratos-ddd/api/rpc"
 	"github.com/mfslog/kratos-ddd/infra/conf"
 	"go.etcd.io/etcd/clientv3"
-	"net"
 	"os"
 	"strings"
 	"time"
@@ -34,7 +34,7 @@ type App struct {
 func NewApp(svcHandler *rpc.Handler, g *warden.Server, config *conf.Config) (app *App, closeFunc func(), err error) {
 	var (
 		unregister context.CancelFunc
-		ip         string
+		internalIP string
 	)
 	app = &App{
 		svcHandler: svcHandler,
@@ -44,16 +44,13 @@ func NewApp(svcHandler *rpc.Handler, g *warden.Server, config *conf.Config) (app
 
 	//服务注册
 	hostName, _ := os.Hostname()
-	ip, err = app.getLocalIP()
-	if err != nil {
-		return
-	}
+	internalIP = ip.InternalIP()
 	addrInfo := strings.Split(config.Grpc.Addr, ":")
 	if len(addrInfo) != 2 {
 		err = errors.New("server addr config error :" + config.Grpc.Addr)
 		return
 	}
-	addrs := []string{fmt.Sprintf("%s:%s", ip, addrInfo[1])}
+	addrs := []string{fmt.Sprintf("%s:%s", internalIP,addrInfo[1])}
 	ins := &naming.Instance{
 		Region:   env.Region,
 		Zone:     env.Zone,
@@ -99,29 +96,5 @@ func (a *App) registerEtcd(ins *naming.Instance) (unregister context.CancelFunc,
 
 	nsEtcd, err = etcd.New(cfg)
 	unregister, err = nsEtcd.Register(context.Background(), ins)
-	return
-}
-
-func (a *App) getLocalIP() (addr string, err error) {
-	var (
-		netInterfaces []net.Interface
-	)
-	netInterfaces, err = net.Interfaces()
-	if err != nil {
-		return
-	}
-	for i := 0; i < len(netInterfaces); i++ {
-		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
-			addrs, _ := netInterfaces[i].Addrs()
-			for _, address := range addrs {
-				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					if ipnet.IP.To4() != nil {
-						addr = ipnet.IP.String()
-						return
-					}
-				}
-			}
-		}
-	}
 	return
 }

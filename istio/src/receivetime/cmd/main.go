2d7c3040950da8564a1e8039e2f3208389e6f0f6
JerryZhou <jerryzhou@boyaa.com>
2020-08-21 20:26:02 +0800

    feat:增加receivetime服务

--- istio/src/receivetime/cmd/main.go
+++ istio/src/receivetime/cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
-	"log"
+	"github.com/sirupsen/logrus"
 	"net"
-
+	pb "github.com/JerryZhou343/receivetime/genproto/github.com/JerryZhou343/lab/istio/receivetime"
 	"google.golang.org/grpc"
-	pb "github.com/JerryZhou343/echo/genproto/github.com/JerryZhou343/lab/istio/echo"
+	"time"
 )
 
 const (
@@ -15,23 +15,28 @@ const (
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedTimeServerServer
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+func (s *server) SayHello(ctx context.Context, in *pb.GetCurrentTimeRequest) (*pb.GetCurrentTimeReply, error) {
+	logrus.Infof("have get request")
+	//return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+	at := time.Now().Unix()
+	return &pb.GetCurrentTimeReply{
+		CurrentAt:            at,
+	},nil
 }
 
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		logrus.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	//pb.RegisterGreeterServer(s, &server{})
+
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		logrus.Fatalf("failed to serve: %v", err)
 	}
 }
--- istio/src/receivetime/go.mod
+++ istio/src/receivetime/go.mod
@@ -1,8 +1,9 @@
-module github.com/JerryZhou343/echo
+module github.com/JerryZhou343/receivetime
 
 go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/sirupsen/logrus v1.6.0
 	google.golang.org/grpc v1.31.0
 )
